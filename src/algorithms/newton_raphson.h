f00a22f22e90b03c2902f61245285957bf328e7c
Akarsh Goyal <akarshg14@gmail.com>
2020-06-19 04:19:30 +0530

    ADMM & FISTA both working(except intercept mismatch with SLOPE::SLOPE() )

--- src/algorithms/newton_raphson.h
+++ src/algorithms/newton_raphson.h
@@ -11,15 +11,16 @@ using namespace arma;
 template <typename T>
 mat Family::newton_raphson(const T& x, const mat&y, const double rho, const mat& u, bool quasi){
 
+  Rcout.precision(4);
   uword p = x.n_cols;
 
   mat z(u);
   mat g(u);
   mat h(p,p);
 
-  uword max_iter = 20;
-  double tolerance = 1e-8;
-  double alpha = 0.2;
+  uword max_iter = 50;
+  double tolerance = 1e-16;
+  double alpha = 0.1;
   double gamma = 0.5;
 
   uword i;
@@ -31,52 +32,31 @@ mat Family::newton_raphson(const T& x, const mat&y, const double rho, const mat&
     g = gradient(x,y,lin_pred)+rho*(z-u);
     h = hessian(x,y,lin_pred);
 
+
     h.diag() += rho;
 
-    mat step = solve(h,g);
+    mat step = -solve(h,g);
     double decrement = dot(g,step);
 
-    // Rcout << "Decrement:" << decrement << "  Newton Passes: " << i << endl;
-    // g.t().print();
-    // step.t().print();
-    
-    if( decrement < tolerance){
+    if( decrement*decrement < tolerance){
       break; 
     }
 
-    //Backtracking; Doesn't work right now.
+    //Backtracking
     double t = 1.0;
 
-    // double f = primal(y,lin_pred)+(rho)/(2.0)*norm(z-u,2);
-    
-    // while( primal(y,x*(z-t*step))+(rho)/(2.0)*norm(z-t*step-u,2) 
-    //         > (f - alpha*t*decrement) ){
-    //     t = gamma*t;
-    //     Rcpp::checkUserInterrupt();
-    // }
-            
-    z = z - t*step;
+    double f = primal(y,lin_pred)+(rho)/(2.0)*pow(norm(z-u),2);
+
+    while( primal(y,x*(z+t*step))+(rho)/(2.0)*pow(norm(z+t*step-u),2)
+            > (f + alpha*t*decrement) ){
+        t = gamma*t;
+        Rcpp::checkUserInterrupt();
+    }
+
+    z = z + t*step;
 
     Rcpp::checkUserInterrupt();
   }
-
-  mat xTx ;
-  xTx = x.t()*x;
-  xTx.diag() += rho;
-
-  // if( !(z-solve(xTx,x.t()*y+rho*(u))).is_zero(1e-4) ){
-  //   Rcout << "Messed up " << endl;
-  //   z.print();
-  //   Rcout << "----------" << endl;
-  //   solve(xTx,x.t()*y+rho*(z-u)).print();
-  //   Rcout << "----------" << endl;
-  //   (z-solve(xTx,x.t()*y+rho*(u))).print();
-  //   Rcout << "----------" << endl;
-  //   u.print();
-  //   Rcout << "----------" << endl;
-  // }
-
-  // Rcout << " Total Newton Passes: " << i << endl;
 
   return z;
 }
