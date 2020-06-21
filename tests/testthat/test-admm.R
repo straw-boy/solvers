0fd61e3b3d629822beb45372fbbad952a39086df
Akarsh Goyal <akarshg14@gmail.com>
2020-06-22 01:23:39 +0530

    Finished changes; build is success

--- tests/testthat/test-admm.R
+++ tests/testthat/test-admm.R
@@ -1,52 +1,52 @@
-# test_that("ADMM (as well as Newton-Raphson) works same as SLOPE package (excluding intercept) ", {
-#   library(SLOPE)
+test_that("ADMM (as well as Newton-Raphson) works same as SLOPE package (excluding intercept) ", {
+  library(SLOPE)
 
-#   fista_slope <- SLOPE(bodyfat$x, bodyfat$y, solver = "fista",intercept=FALSE)
-#   admm_solvers <- ADMM(bodyfat$x, bodyfat$y,intercept=FALSE)
-#   expect_equivalent(coef(fista_slope), coef(admm_solvers), tol = 1e-2)
+  fista_slope <- SLOPE(bodyfat$x, bodyfat$y, solver = "fista",intercept=FALSE)
+  admm_solvers <- ADMM(bodyfat$x, bodyfat$y,intercept=FALSE)
+  expect_equivalent(coef(fista_slope), coef(admm_solvers), tol = 1e-2)
 
-#   fista_slope <- SLOPE(heart$x, heart$y,family="binomial", solver = "fista",intercept=FALSE)
-#   admm_solvers <- ADMM(heart$x, heart$y,family="binomial",intercept=FALSE)
-#   expect_equivalent(coef(fista_slope), coef(admm_solvers), tol = 1e-2)
+  fista_slope <- SLOPE(heart$x, heart$y,family="binomial", solver = "fista",intercept=FALSE)
+  admm_solvers <- ADMM(heart$x, heart$y,family="binomial",intercept=FALSE)
+  expect_equivalent(coef(fista_slope), coef(admm_solvers), tol = 1e-2)
 
-#   fista_slope <- SLOPE(abalone$x, abalone$y,family="poisson", solver = "fista",intercept=FALSE)
-#   admm_solvers <- ADMM(abalone$x, abalone$y,family="poisson",intercept=FALSE)
-#   expect_equivalent(coef(fista_slope), coef(admm_solvers), tol = 1e-2)
-# })
+  fista_slope <- SLOPE(abalone$x, abalone$y,family="poisson", solver = "fista",intercept=FALSE)
+  admm_solvers <- ADMM(abalone$x, abalone$y,family="poisson",intercept=FALSE)
+  expect_equivalent(coef(fista_slope), coef(admm_solvers), tol = 1e-2)
+})
 
-# test_that("ADMM (as well as Newton-Raphson) works (including intercept) ", {
-#   library(SLOPE)
+test_that("ADMM (as well as Newton-Raphson) works (including intercept) ", {
+  library(SLOPE)
 
-#   fista_solvers <- FISTA(bodyfat$x, bodyfat$y, solver = "fista")
-#   admm_solvers <- ADMM(bodyfat$x, bodyfat$y)
-#   expect_equivalent(coef(fista_solvers), coef(admm_solvers), tol = 1e-2)
+  fista_solvers <- FISTA(bodyfat$x, bodyfat$y, solver = "fista")
+  admm_solvers <- ADMM(bodyfat$x, bodyfat$y)
+  expect_equivalent(coef(fista_solvers), coef(admm_solvers), tol = 1e-2)
 
-#   fista_solvers <- FISTA(heart$x, heart$y,family="binomial", solver = "fista")
-#   admm_solvers <- ADMM(heart$x, heart$y,family="binomial")
-#   expect_equivalent(coef(fista_solvers), coef(admm_solvers), tol = 1e-2)
+  fista_solvers <- FISTA(heart$x, heart$y,family="binomial", solver = "fista")
+  admm_solvers <- ADMM(heart$x, heart$y,family="binomial")
+  expect_equivalent(coef(fista_solvers), coef(admm_solvers), tol = 1e-2)
 
-#   fista_solvers <- FISTA(abalone$x, abalone$y,family="poisson", solver = "fista")
-#   admm_solvers <- ADMM(abalone$x, abalone$y,family="poisson")
-#   expect_equivalent(coef(fista_solvers), coef(admm_solvers), tol = 1e-2)
-# })
+  fista_solvers <- FISTA(abalone$x, abalone$y,family="poisson", solver = "fista")
+  admm_solvers <- ADMM(abalone$x, abalone$y,family="poisson")
+  expect_equivalent(coef(fista_solvers), coef(admm_solvers), tol = 1e-2)
+})
 
 
-# test_that("ADMM (as well as Newton-Raphson) works same as SLOPE package (including intercept) ", {
-#   skip('Intercept mismatch in this')
+test_that("ADMM (as well as Newton-Raphson) works same as SLOPE package (including intercept) ", {
+  skip('Intercept mismatch in this')
 
-#   library(SLOPE)
+  library(SLOPE)
 
-#   fista_slope <- SLOPE(bodyfat$x, bodyfat$y, solver = "fista")
-#   admm_solvers <- ADMM(bodyfat$x, bodyfat$y)
-#   expect_equivalent(coef(fista_slope), coef(admm_solvers), tol = 1e-2)
+  fista_slope <- SLOPE(bodyfat$x, bodyfat$y, solver = "fista")
+  admm_solvers <- ADMM(bodyfat$x, bodyfat$y)
+  expect_equivalent(coef(fista_slope), coef(admm_solvers), tol = 1e-2)
 
-#   fista_slope <- SLOPE(heart$x, heart$y,family="binomial", solver = "fista")
-#   admm_solvers <- ADMM(heart$x, heart$y,family="binomial")
-#   expect_equivalent(coef(fista_slope), coef(admm_solvers), tol = 1e-2)
+  fista_slope <- SLOPE(heart$x, heart$y,family="binomial", solver = "fista")
+  admm_solvers <- ADMM(heart$x, heart$y,family="binomial")
+  expect_equivalent(coef(fista_slope), coef(admm_solvers), tol = 1e-2)
 
-#   fista_slope <- SLOPE(abalone$x, abalone$y,family="poisson", solver = "fista")
-#   admm_solvers <- ADMM(abalone$x, abalone$y,family="poisson")
-#   expect_equivalent(coef(fista_slope), coef(admm_solvers), tol = 1e-2)
-# })
+  fista_slope <- SLOPE(abalone$x, abalone$y,family="poisson", solver = "fista")
+  admm_solvers <- ADMM(abalone$x, abalone$y,family="poisson")
+  expect_equivalent(coef(fista_slope), coef(admm_solvers), tol = 1e-2)
+})
 
 
