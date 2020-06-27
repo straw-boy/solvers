83661f83c61b79e531559bc5dce1736a2e295f60
Akarsh Goyal <akarshg14@gmail.com>
2020-06-28 01:41:36 +0530

    added fista tests for poisson case

--- tests/testthat/test-fista.R
+++ tests/testthat/test-fista.R
@@ -7,8 +7,8 @@ test_that("FISTA: gaussian, n>p case", {
 
   d <- randomProblem(n,p,response="gaussian")
   
-  fista_solvers <- FISTA(d$x, d$y, family="gaussian",path_length=3)
-  fista_slope <- SLOPE(d$x, d$y, family="gaussian",path_length=3)
+  fista_solvers <- FISTA(d$x, d$y, family="gaussian",alpha=c(1.0,0.005))
+  fista_slope <- SLOPE(d$x, d$y, family="gaussian",alpha=c(1.0,0.005))
   expect_equivalent(coef(fista_solvers), coef(fista_slope), tol = 1e-2)
 
 })
@@ -18,13 +18,13 @@ test_that("FISTA: gaussian, n<p case", {
   set.seed(1)
 
 
-  n = 10
+  n = 5
   p = 20
 
   d <- randomProblem(n,p,response="gaussian")
   
-  fista_solvers <- FISTA(d$x, d$y, family="gaussian",path_length=3)
-  fista_slope <- SLOPE(d$x, d$y, family="gaussian",path_length=3)
+  fista_solvers <- FISTA(d$x, d$y, family="gaussian",alpha=c(1.0,0.005))
+  fista_slope <- SLOPE(d$x, d$y, family="gaussian",alpha=c(1.0,0.005))
   expect_equivalent(coef(fista_solvers), coef(fista_slope), tol = 1e-2)
 
 })
@@ -41,8 +41,8 @@ test_that("FISTA: binomial, n>p case", {
 
   d <- randomProblem(n,p,response="binomial")
   
-  fista_solvers <- FISTA(d$x, d$y, family="binomial",path_length=3)
-  fista_slope <- SLOPE(d$x, d$y, family="binomial",path_length=3)
+  fista_solvers <- FISTA(d$x, d$y, family="binomial",alpha=c(1.0,0.005))
+  fista_slope <- SLOPE(d$x, d$y, family="binomial",alpha=c(1.0,0.005))
   
   expect_equivalent(coef(fista_solvers), coef(fista_slope), tol = 1e-2)
 
@@ -57,11 +57,47 @@ test_that("FISTA: binomial, n<p case", {
   p = 20
 
   d <- randomProblem(n,p,response="binomial")
+  print(d)
   
-  fista_solvers <- FISTA(d$x, d$y, family="binomial",path_length=3)
-  fista_slope <- SLOPE(d$x, d$y, family="binomial",path_length=3)
+  fista_solvers <- FISTA(d$x, d$y, family="binomial",alpha=c(1.0,0.005))
+  fista_slope <- SLOPE(d$x, d$y, family="binomial",alpha=c(1.0,0.005))
 
   expect_equivalent(coef(fista_solvers), coef(fista_slope), tol = 1e-2)
 
 })
 
+test_that("FISTA: poisson, n>p case", {
+  skip('Intercept mismatch in this')
+
+  library(SLOPE)
+  set.seed(1)
+
+  n = 100
+  p = 10
+
+  d <- randomProblem(n,p,response="poisson")
+  
+  fista_solvers <- FISTA(d$x, d$y, family="poisson",alpha=c(1.0,0.005))
+  fista_slope <- SLOPE(d$x, d$y, family="poisson",alpha=c(1.0,0.005))
+
+  expect_equivalent(coef(fista_solvers), coef(fista_slope), tol = 1e-2)
+
+})
+
+test_that("FISTA: poisson, n<p case", {
+  skip('Intercept mismatch in this')
+
+  library(SLOPE)
+  set.seed(1)
+
+  n = 10
+  p = 20
+
+  d <- randomProblem(n,p,response="poisson")
+  
+  fista_solvers <- FISTA(d$x, d$y, family="poisson",alpha=c(1.0,0.005))
+  fista_slope <- SLOPE(d$x, d$y, family="poisson",alpha=c(1.0,0.005))
+
+  expect_equivalent(coef(fista_solvers), coef(fista_slope), tol = 1e-2)
+
+})
