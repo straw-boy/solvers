736bceaa10b0ffbe0926ec018d8fa839b7683202
Akarsh Goyal <akarshg14@gmail.com>
2020-07-01 18:48:21 +0530

    added more tests

---
+++ tests/testthat/test-bfgs.R
@@ -0,0 +1,95 @@
+test_that("BFGS: gaussian, n>p case", {
+  library(SLOPE)
+  set.seed(1)
+
+  n = 100
+  p = 10
+
+  d <- randomProblem(n,p,response="gaussian")
+  
+  nr <- ADMM(d$x, d$y, family="gaussian", alpha=c(1.0,0.005), opt_algo="nr")
+  bfgs <- ADMM(d$x, d$y, family="gaussian",alpha=c(1.0,0.005), opt_algo="bfgs")
+  expect_equivalent(coef(nr), coef(bfgs), tol = 1e-2)
+
+})
+
+test_that("BFGS: gaussian, n<p case", {
+
+  library(SLOPE)
+  set.seed(1)
+
+  n = 10
+  p = 20
+
+  d <- randomProblem(n,p,response="gaussian")
+  
+  nr <- ADMM(d$x, d$y, family="gaussian", alpha=c(1.0,0.005), opt_algo="nr")
+  bfgs <- ADMM(d$x, d$y, family="gaussian",alpha=c(1.0,0.005), opt_algo="bfgs")
+  expect_equivalent(coef(nr), coef(bfgs), tol = 1e-2)
+
+})
+
+test_that("BFGS: binomial, n>p case", {
+  
+  library(SLOPE)
+  set.seed(1)
+
+  n = 100
+  p = 10
+
+  d <- randomProblem(n,p,response="binomial")
+  
+  nr <- ADMM(d$x, d$y, family="binomial", alpha=c(1.0,0.005), opt_algo="nr")
+  bfgs <- ADMM(d$x, d$y, family="binomial",alpha=c(1.0,0.005), opt_algo="bfgs")
+  expect_equivalent(coef(nr), coef(bfgs), tol = 1e-2)
+
+})
+
+
+test_that("BFGS: binomial, n<p case", {
+
+  library(SLOPE)
+  set.seed(1)
+
+  n = 10
+  p = 20
+
+  d <- randomProblem(n,p,response="binomial")
+  
+  nr <- ADMM(d$x, d$y, family="binomial", alpha=c(1.0,0.005), opt_algo="nr")
+  bfgs <- ADMM(d$x, d$y, family="binomial",alpha=c(1.0,0.005), opt_algo="bfgs")
+  expect_equivalent(coef(nr), coef(bfgs), tol = 1e-2)
+
+})
+
+test_that("BFGS: poisson, n>p case", {
+
+  library(SLOPE)
+  set.seed(1)
+
+  n = 100
+  p = 10
+
+  d <- randomProblem(n,p,response="poisson")
+  
+  nr <- ADMM(d$x, d$y, family="poisson", alpha=c(1.0,0.005), opt_algo="nr")
+  bfgs <- ADMM(d$x, d$y, family="poisson",alpha=c(1.0,0.005), opt_algo="bfgs")
+  expect_equivalent(coef(nr), coef(bfgs), tol = 1e-2)
+})
+
+test_that("BFGS: poisson, n<p case", {
+
+  library(SLOPE)
+  set.seed(1)
+
+  n = 10
+  p = 20
+
+  d <- randomProblem(n,p,response="poisson")
+  
+  nr <- ADMM(d$x, d$y, family="poisson", alpha=c(1.0,0.005), opt_algo="nr")
+  bfgs <- ADMM(d$x, d$y, family="poisson",alpha=c(1.0,0.005), opt_algo="bfgs")
+  expect_equivalent(coef(nr), coef(bfgs), tol = 1e-2)
+
+})
+
