04367ab42b54f1495ddc96f570b7f780d071c8c4
Akarsh Goyal <akarshg14@gmail.com>
2020-07-26 19:59:45 +0530

    corrected norm in admm.h

--- src/algorithms/admm.h
+++ src/algorithms/admm.h
@@ -61,11 +61,11 @@ Results Family::fitADMM(const T& x, const mat& y, vec lambda, const std::string 
 
     u += (beta_hat-z);
 
-    double r_norm = norm(beta - z);
-    double s_norm = norm(rho*(z - z_old));
-
-    double eps_primal = std::sqrt(p*m)*tol_abs + tol_rel*std::max(norm(beta), norm(z));
-    double eps_dual = std::sqrt(p*m)*tol_abs + tol_rel*norm(rho*u);
+    double r_norm = std::sqrt(accu(square(beta - z)));
+    double s_norm = std::sqrt(accu(square(z - z_old)));
+    
+    double eps_primal = std::sqrt(p*m)*tol_abs + tol_rel*std::max(std::sqrt(accu(square(beta))), std::sqrt(accu(square(z))));
+    double eps_dual = std::sqrt(p*m)*tol_abs + tol_rel*std::sqrt(accu(square(rho*u)));
 
     if (diagnostics) {
       eps_primals.push_back(r_norm);
